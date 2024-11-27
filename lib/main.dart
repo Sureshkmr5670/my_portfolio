0560d429e3815127b8fc61ba5e7c9814cf33e15c
surekmr007 <surekmr007@gmail.com>
2024-11-27 21:35:10 +0530

    for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:my_portfolio/common/widgets/mesh_gradient.dart';
-import 'package:my_portfolio/data/services/portfolio_service.dart';
-import 'package:my_portfolio/features/authentication/controllers/navigation_index.dart';
-import 'package:my_portfolio/features/authentication/controllers/portfolio_provider.dart';
-import 'package:my_portfolio/portfolio.dart';
-import 'package:my_portfolio/utils/theme/theme.dart';
-import 'package:provider/provider.dart';
 
-void main() async {
-  // Ensure this is called only once
-  WidgetsFlutterBinding.ensureInitialized();
+void main() => runApp(MyApp());
 
-  final portfolioService = PortfolioService();
-  final portfolioProvider =
-      PortfolioProvider(portfolioService: portfolioService);
-
-  try {
-    await portfolioProvider.initializeData();
-  } catch (e) {
-    // Handle initialization errors
-    print('Initialization error: $e');
-  }
-
-  FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
-  FlutterNativeSplash.remove();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => NavigationProvider(),
-      child: MyApp(
-        portfolioService: portfolioService,
-        portfolioProvider: portfolioProvider,
-      ),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  final PortfolioService portfolioService;
-  final PortfolioProvider portfolioProvider;
-
-  const MyApp({
-    super.key,
-    required this.portfolioService,
-    required this.portfolioProvider,
-  });
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      builder: (context, child) => MultiProvider(
-        providers: [
-          Provider<PortfolioService>.value(
-            value: portfolioService,
-          ),
-          ChangeNotifierProvider<PortfolioProvider>.value(
-            value: portfolioProvider,
-          ),
-        ],
-        child: GetMaterialApp(
-          title: 'Portfolio Website',
-          theme: CustomAppTheme.lightTheme,
-          darkTheme: CustomAppTheme.darkTheme,
-          themeMode: ThemeMode.dark,
-          home:
-              Scaffold(body: MeshGradientBackground(child: PortfolioScreen())),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('hello'),
         ),
       ),
     );

5cbcda781b0f11536c7238cce392aaf2f5d1187b
surekmr007 <surekmr007@gmail.com>
2024-11-27 22:06:15 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:my_portfolio/common/widgets/mesh_gradient.dart';
+import 'package:my_portfolio/data/services/portfolio_service.dart';
 import 'package:my_portfolio/features/authentication/controllers/navigation_index.dart';
+import 'package:my_portfolio/features/authentication/controllers/portfolio_provider.dart';
 import 'package:my_portfolio/utils/theme/theme.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   // Ensure this is called only once
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // final portfolioService = PortfolioService();
-  // final portfolioProvider =
-  //     PortfolioProvider(portfolioService: portfolioService);
+  final portfolioService = PortfolioService();
+  final portfolioProvider =
+      PortfolioProvider(portfolioService: portfolioService);
 
-  // try {
-  //   await portfolioProvider.initializeData();
-  // } catch (e) {
-  //   // Handle initialization errors
-  //   print('Initialization error: $e');
-  // }
+  try {
+    await portfolioProvider.initializeData();
+  } catch (e) {
+    // Handle initialization errors
+    print('Initialization error: $e');
+  }
 
-  // FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
-  // FlutterNativeSplash.remove();
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
+  FlutterNativeSplash.remove();
 
   runApp(
     ChangeNotifierProvider(
